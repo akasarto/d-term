2b5295945499ddd8af0a9c5baf4eeba4bf4820a8
akasarto <github@thiagoschneider.com>
2018-02-02 20:12:41 -0200

    Layout review

--- sources/dTerm.UI.Wpf/Shell/ShellViewModel.cs
+++ sources/dTerm.UI.Wpf/Shell/ShellViewModel.cs
@@ -17,15 +17,6 @@ namespace dTerm.UI.Wpf.Shell
 			CreateConsole = ReactiveCommand.Create(CreateConsoleExecute);
 		}
 
-
-		private ConsoleHwndHost _tmp = null;
-		public ConsoleHwndHost Tmp
-		{
-			get => _tmp;
-			set => this.RaiseAndSetIfChanged(ref _tmp, value);
-		}
-
-
 		public ReactiveList<ConsoleInstanceViewModel> ConsoleInstances { get; set; } = new ReactiveList<ConsoleInstanceViewModel>();
 
 		public ReactiveCommand CreateConsole { get; protected set; }
@@ -40,13 +31,12 @@ namespace dTerm.UI.Wpf.Shell
 
 			consoleProcess.Start();
 
-			Tmp = new ConsoleHwndHost(consoleProcess);
-			//var consoleInstanceViewModel = new ConsoleInstanceViewModel(consoleProcess)
-			//{
-			//	Name = DateTime.Now.Millisecond.ToString()
-			//};
+			var consoleInstanceViewModel = new ConsoleInstanceViewModel(consoleProcess)
+			{
+				Name = DateTime.Now.Millisecond.ToString()
+			};
 
-			//ConsoleInstances.Add(consoleInstanceViewModel);
+			ConsoleInstances.Add(consoleInstanceViewModel);
 		}
 	}
 }
