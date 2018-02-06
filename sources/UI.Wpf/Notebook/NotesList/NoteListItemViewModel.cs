9ed6b2fe6249a9e039885fd8f2ffadb6ea26af0d
akasarto <github@thiagoschneider.com>
2018-02-06 16:39:08 -0200

    Constrols review

--- sources/UI.Wpf/Notebook/NotesList/NoteListItemViewModel.cs
+++ sources/UI.Wpf/Notebook/NotesList/NoteListItemViewModel.cs
@@ -1,14 +1,43 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 
 namespace UI.Wpf.Notebook
 {
-	public class NoteListItemViewModel
+	public class NoteListItemViewModel : ReactiveObject
 	{
-		public bool Adding { get; set; }
+		private bool _isEditing = false;
+
+		public NoteListItemViewModel()
+		{
+			EditCommand = ReactiveCommand.Create(() =>
+			{
+				IsEditing = true;
+			});
+
+			CancelCommand = ReactiveCommand.Create(() =>
+			{
+				IsEditing = false;
+			});
+
+			SaveCommand = ReactiveCommand.Create(() =>
+			{
+				System.Windows.MessageBox.Show("Save");
+			});
+		}
 
 		public Guid Id { get; set; }
 		public int Index { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
+
+		public bool IsEditing
+		{
+			get => _isEditing;
+			set => this.RaiseAndSetIfChanged(ref _isEditing, value);
+		}
+
+		public ReactiveCommand EditCommand { get; protected set; }
+		public ReactiveCommand CancelCommand { get; protected set; }
+		public ReactiveCommand SaveCommand { get; protected set; }
 	}
 }
